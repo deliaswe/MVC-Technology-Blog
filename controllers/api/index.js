56393bf0711b14510a869f02ba5e58bf56468d49
delia <you@example.com>
2023-07-18 17:17:11 -0400

    set up the routes

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
+
+// set up the routes
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+// export the module
+module.exports = router;
