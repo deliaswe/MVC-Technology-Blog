b7a5268a9c4bc9f861e83fdbe0fde3e0226618e0
delia <you@example.com>
2023-07-30 00:39:06 -0400

    comments

---
+++ seeds/commentData.js
@@ -0,0 +1,40 @@
+const { Comment } = require('../models');
+const commentData = [
+    {
+        comment_text: 'Great car!',
+        user_id: 1,
+        post_id: 1,
+    },
+    {
+        comment_text: 'I love this car!',
+        user_id: 2,
+        post_id: 1,
+    },
+    {
+        comment_text: 'I want this car!',
+        user_id: 3,
+        post_id: 1,
+    },
+    {
+        comment_text: 'I want this car!',
+        user_id: 4,
+        post_id: 1,
+    },
+    {
+        comment_text: 'I want this car!',
+        user_id: 5,
+        post_id: 1,
+    },
+    {
+        comment_text: 'I want this car!',
+        user_id: 1,
+        post_id: 2,
+    },
+    {
+        comment_text: 'I want this car!',
+        user_id: 2,
+        post_id: 2,
+    },
+];
+const seedComments = () => Comment.bulkCreate(commentData);
+module.exports = seedComments;
