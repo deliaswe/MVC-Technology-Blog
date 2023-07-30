ca37a21b9f70ffbbe9a13211c3e5fb94bb27c367
delia <you@example.com>
2023-07-30 00:21:19 -0400

    setting up session

---
+++ server.js
@@ -0,0 +1,54 @@
+// importing required dependencies and models
+const express = require('express');
+const session = require('express-session');
+const SequelizStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers: require('./utils/helpers') });
+
+// setting up express and port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// setting up session
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizStore({
+        db: sequelize,
+    }),
+};
+
+// using session
+app.use(session(sess));
+
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// for public folder
+app.use(express.static(path.join('public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// using session middleware
+app.use(
+    session({
+        secret: process.env.DB_SESSION_SECRET,
+        store: new SequelizStore({ db: sequelize }),
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// using routes
+app.use(routes);
+
+// syncing sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+}
+);
