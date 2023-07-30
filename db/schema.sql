ba29b12607647fb9d4c791186ce57b0d16cae1a4
delia <you@example.com>
2023-07-30 15:31:22 -0400

    removed changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,2 @@
 DROP DATABASE IF EXISTS Delia_CarBlog;
 CREATE DATABASE Delia_CarBlog;
-
-USE Delia_CarBlog;
-
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-);
-
-CREATE TABLE posts (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50) NOT NULL,
-    content TEXT NOT NULL,
-    created_at DATETIME NOT NULL,
-    user_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
